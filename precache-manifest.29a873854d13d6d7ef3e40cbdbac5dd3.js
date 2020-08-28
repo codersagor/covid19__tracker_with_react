f63b3edfeabc7d808f5b4a450ef4a01c4e9ebf7c
unknown <nur839184@gmail.com>
2020-08-29 00:11:39 +0600

    First commit by coder Sagor

---
+++ precache-manifest.29a873854d13d6d7ef3e40cbdbac5dd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbf8e6bfc3cacca469744077c90f1eaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36effb28695de2dc8e81",
+    "url": "/static/css/main.e9eee847.chunk.css"
+  },
+  {
+    "revision": "8fb6aea26935120ab18a",
+    "url": "/static/js/2.2272f125.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/static/js/2.2272f125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36effb28695de2dc8e81",
+    "url": "/static/js/main.16166acc.chunk.js"
+  },
+  {
+    "revision": "78be55391bfcc0776429",
+    "url": "/static/js/runtime-main.9af3c916.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
